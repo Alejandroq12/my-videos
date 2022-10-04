1a9630a19e8772438bd4f8db650fc0d3db8c6bfa
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-10-03 23:55:25 -0600

    UpdatingComponents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
 
-const App = () => {
-    return (
-        <div>App</div>
-    )
-}
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />}/>
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+ </BrowserRouter>
+);
 
 export default App

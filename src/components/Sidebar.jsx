f438b198d6de089c083c824003f4a11ccff5bb1e
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-10-04 20:21:38 -0600

    addingSideBar
    I added different categories inside the Sidebar. I added style, and different functionalities.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import { Stack } from '@mui/material';
 
-const Sidebar = () => {
-    return (
-        <div>Sidebar</div>
-    )
-}
+import { categories } from './utils/constants';
+
+const selectedCategory = 'New';
+
+const Sidebar = () => (
+        <Stack
+        direction="row"
+        sx={{
+            overflowY: "auto",
+            height: { sx: 'auto', md: '95%' },
+            flexDirection: {md: 'column'},
+        }}
+        >
+           {categories.map((category) => (
+            <button
+            className="category-btn"
+            style={{
+                background: category.name === selectedCategory && '#FC1503',
+                color: 'white'
+            }}
+            key={category.name}
+            >
+            <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
+            <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
+            </button>
+           ))}
+        </Stack>
+)
 
 export default Sidebar

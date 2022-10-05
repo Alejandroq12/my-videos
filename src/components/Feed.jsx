d2fbe9fa31b40a6bfe851634d95f35e19fd3ee7d
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-10-04 19:25:33 -0600

    addingFeed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
 
 const Feed = () => {
     return (
-        <div>Feed</div>
+        <Stack sx={{flexDirection: { sx:
+         "column", md: "row"  } }}>
+           <Box sx={{ height: { sx: 'auto', md:
+            '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
+             <Sidebar />
+             <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
+               Copyright 2022 Julio Quezada - Software Developer.
+             </Typography>
+           </Box>
+        </Stack>
     )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as ChannelDetail } from './ChannelDetail';
 export { default as SearchFeed } from './SearchFeed';
 export { default as VideoDetail } from './VideoDetail';
 export { default as SearchBar } from './SearchBar';
+export { default as Sidebar } from './Sidebar';
